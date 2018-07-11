c28e6cc2bc9d691fd6a62059016d0aea854f7f48
Florine Kaino <41108630+Fkaino@users.noreply.github.com>
2018-07-11 15:59:22 +0300

    Create Fkaino.py

---
+++ Fkaino.py
@@ -0,0 +1,20 @@
+Darasa=["Florine","Tony","Edwin","Sharon","Christabel","Brenda"]
+Darasa.append("Abel")
+
+print(Darasa)
+
+Darasa.remove ("Edwin")
+print(Darasa)
+Standard1=list(Darasa)
+print(Standard1)
+
+Standard2=["Ann","Brian","Ben","Lewis","Shirley","Faith"]
+
+Standard3=["Maya","Lilie","Kevin","Cliff","Mwai","Phil","Eve","Joy","Diana"]    
+
+VogueSchool=[Standard1,Standard2,Standard3]
+
+Standard4=["Adrian","Molly","Frank","Mike","Naomi","Njeri","Alfred"]
+VogueSchool.append(Standard4)
+
+print(VogueSchool) 
